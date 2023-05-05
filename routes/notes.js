b6dc0ef892b73d95cdc1bd1a5c66e83cd2d5316a
morrispg <124636880+morrispg@users.noreply.github.com>
2023-05-05 09:42:21 -0500

    Initial commit.

---
+++ routes/notes.js
@@ -0,0 +1,55 @@
+const notes = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const {
+  readFromFile,
+  readAndAppend,
+  writeToFile,
+} = require('../helpers/fsUtils');
+
+// GET Route for retrieving all the notes
+// http://localhost:3001/api/notes/
+notes.get('/', (req, res) => {
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// GET Route for a specific notes
+
+
+// DELETE Route for a specific notes
+notes.delete('/:notes_id', (req, res) => {
+  const notesId = req.params.notes_id;
+  readFromFile('./db/notes.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      // Make a new array of all notess except the one with the ID provided in the URL
+      const result = json.filter((notes) => notes.notes_id !== notesId);
+
+      // Save that array to the filesystem
+      writeToFile('./db/notes.json', result);
+
+      // Respond to the DELETE request
+      res.json(`Item ${notesId} has been deleted 🗑️`);
+    });
+});
+
+// POST Route for a new UX/UI notes
+// http://localhost:3001/api/notes/
+notes.post('/', (req, res) => {
+  console.log(req.body);
+
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newnotes = {
+      title, text,
+      id: uuidv4(),
+    };
+
+    const parseData =readAndAppend(newnotes, './db/db.json');
+    res.json(parseData);
+  } else {
+    res.error('Error in adding notes');
+  }
+});
+
+module.exports = notes;
---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const htmlRoute = require('./routes/htmlRoutes')
+
+const app = express()
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(express.static("public"))
+app.use("/",htmlRoute)
+app.listen(PORT, ()=> console.log(`listening on PORT ${PORT}`))
